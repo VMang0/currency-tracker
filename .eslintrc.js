4e28c1c7ac3214992b95d0de14893870ec3850ae
VMang0 <valeriakorolkova2703@gmail.com>
2024-06-04 20:00:29 +0300

    initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,75 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier',
+    'simple-import-sort',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@components', './src/components'],
+          ['@assets', './src/assets'],
+          ['@pages', './src/pages'],
+          ['@constants', './src/constants'],
+          ['@hooks', './src/hooks'],
+          ['@utils', './src/utils'],
+          ['@styled', './src/styled'],
+          ['@redux', './src/redux'],
+        ],
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/prop-types': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'groups': [
+          'builtin', 'external', 'internal',
+          ['parent', 'sibling', 'index']
+        ],
+        'alphabetize': { "order": 'asc', 'caseInsensitive': true },
+        'newlines-between': 'always',
+      }
+    ],
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@constants': path.resolve(__dirname, 'src/constants'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+        '@styled': path.resolve(__dirname, 'src/styled'),
+        '@redux': path.resolve(__dirname, 'src/redux'),
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack', 'url-loader']
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          type: 'asset/resource'
+        }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      hot: true,
+      open: true,
+      port: 3000
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        favicon: path.resolve(__dirname, './src/assets/icons/favicon.svg'),
+      }),
+      new Dotenv()
+    ]
+  };
+};
