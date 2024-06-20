c4819acd5863933ea253e1bb0f835e9021759a67
VMang0 <valeriakorolkova2703@gmail.com>
2024-06-21 01:05:08 +0300

    feat: currency card information

---
+++ src/constants/currency.ts
@@ -0,0 +1,11 @@
+export const currencyData = [
+  { value: 'USD', label: 'USD - US dollars' },
+  { value: 'ARS', label: 'ARS - Argentine pesos' },
+  { value: 'AUD', label: 'AUD - Australian dollars' },
+  { value: 'BTC', label: 'BTC - Bitcoins' },
+  { value: 'CAD', label: 'CAD - Canadian dollars' },
+  { value: 'CNY', label: 'CNY - Chinese yuan' },
+  { value: 'EUR', label: 'EUR - Euros' },
+  { value: 'GBP', label: 'GBP - British pounds sterling' },
+  { value: 'JPY', label: 'JPY - Japanese yen' },
+];
