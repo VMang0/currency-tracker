aae8452cc441d7f3525dd95b7a489ceedfc1b0bf
VMang0 <valeriakorolkova2703@gmail.com>
2024-06-21 01:20:47 +0300

    refactor: fetch banks test

---
+++ src/constants/period.ts
@@ -0,0 +1,7 @@
+export const periodData = [
+  { value: '1DAY', label: '1 Day' },
+  { value: '5DAYS', label: '5 Days' },
+  { value: '10DAYS', label: '10 Days' },
+  { value: '20DAYS', label: '20 Days' },
+  { value: '1MONTH', label: '1 Month' },
+];
