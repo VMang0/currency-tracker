d117dd69ce3a6f19e919e4f310277630741804ea
VMang0 <valeriakorolkova2703@gmail.com>
2024-06-21 01:22:28 +0300

    test: theme, navigation

---
+++ cypress/e2e/Theme.cy.js
@@ -0,0 +1,23 @@
+import { Path } from '@constants/path';
+import { DATA_TEST_ID } from './data';
+
+describe('Theme Redux Slice', () => {
+  beforeEach(() => {
+    cy.visit(Path.HOME);
+  });
+
+  it('should load with default theme from localStorage', () => {
+    cy.get('body').should('have.css', 'background-color', 'rgb(3, 3, 4)');
+  });
+
+  it('should change theme when toggling', () => {
+    cy.get(`[data-test-id=${DATA_TEST_ID.TOGGLE_THEME}]`).click();
+    cy.get('body').should('have.css', 'background-color', 'rgb(252, 252, 252)');
+  });
+
+  it('should remember selected theme after page reload', () => {
+    cy.get(`[data-test-id=${DATA_TEST_ID.TOGGLE_THEME}]`).click();
+    cy.reload();
+    cy.get('body').should('have.css', 'background-color', 'rgb(252, 252, 252)');
+  });
+});
---
+++ cypress/e2e/data.ts
@@ -0,0 +1,15 @@
+export enum DATA_TEST_ID {
+  HOME_PAGE = 'home-page-id',
+  CONTACTS_PAGE = 'contacts-page-id',
+  TIMELINE_PAGE = 'timeline-page-id',
+  BANK_CARD_PAGE = 'bank-card-page-id',
+  TOGGLE_THEME = 'toggle-theme',
+  LOADER = 'loader',
+  SELECT_CURRENCY = 'select-currency',
+  INPUT_DATE = 'input-date',
+  SELECT_PERIOD = 'select-period',
+  BAR_CHART = 'bar-chart',
+  SELECT = 'select',
+  SELECT_INPUT = 'select-input',
+  SELECT_LIST = 'select-list',
+}
