f336e549a6e06673af770174180cbc5e17114f57
VMang0 <valeriakorolkova2703@gmail.com>
2024-07-01 10:07:41 +0300

    feat(utils): moks data

---
+++ src/utils/data/latestCurrency.ts
@@ -0,0 +1,45 @@
+import { LatestCurrenciesType } from '@type/currency';
+
+export const latestCurrencies: LatestCurrenciesType = {
+  meta: {
+    last_updated_at: '2024-06-22T23:59:59Z',
+  },
+  data: {
+    ARS: {
+      code: 'ARS',
+      value: 905.1666639555,
+    },
+    AUD: {
+      code: 'AUD',
+      value: 1.5053502208,
+    },
+    BTC: {
+      code: 'BTC',
+      value: 0.0000154971,
+    },
+    CAD: {
+      code: 'CAD',
+      value: 1.3691401401,
+    },
+    CNY: {
+      code: 'CNY',
+      value: 7.2581709648,
+    },
+    EUR: {
+      code: 'EUR',
+      value: 0.9343701341,
+    },
+    GBP: {
+      code: 'GBP',
+      value: 0.7904801526,
+    },
+    JPY: {
+      code: 'JPY',
+      value: 159.7687264798,
+    },
+    USD: {
+      code: 'USD',
+      value: 1,
+    },
+  },
+};
