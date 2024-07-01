49e39e365a3d5e813281f9c04642a78211b903d5
VMang0 <valeriakorolkova2703@gmail.com>
2024-07-01 09:44:58 +0300

    refactor(constants): add some constants for links / periods

--- src/constants/period.ts
+++ src/constants/period.ts
@@ -1,7 +1,15 @@
+export enum Period {
+  ONE_DAY = '1DAY',
+  FIVE_DAYS = '5DAYS',
+  TEN_DAYS = '10DAYS',
+  TWENTY_DAYS = '20DAYS',
+  ONE_MONTH = '1MONTH',
+}
+
 export const periodData = [
-  { value: '1DAY', label: '1 Day' },
-  { value: '5DAYS', label: '5 Days' },
-  { value: '10DAYS', label: '10 Days' },
-  { value: '20DAYS', label: '20 Days' },
-  { value: '1MONTH', label: '1 Month' },
+  { value: Period.ONE_DAY, label: '1 Day' },
+  { value: Period.FIVE_DAYS, label: '5 Days' },
+  { value: Period.TEN_DAYS, label: '10 Days' },
+  { value: Period.TWENTY_DAYS, label: '20 Days' },
+  { value: Period.ONE_MONTH, label: '1 Month' },
 ];
