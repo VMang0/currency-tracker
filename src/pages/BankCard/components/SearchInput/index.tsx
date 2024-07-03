f10ce8de4a6d4adf543598fb617cdb377cb565c4
VMang0 <valeriakorolkova2703@gmail.com>
2024-07-04 02:17:39 +0300

    refactor: rename component to SearchInput, remove inline functions

--- src/pages/BankCard/components/SearchInput/index.tsx
+++ src/pages/BankCard/components/SearchInput/index.tsx
@@ -14,23 +14,23 @@ import { setSearchQuery } from '@redux/slices/banksSlice';
 import { filteredOptionsSelector, searchQuerySelector } from '@redux/slices/banksSlice/selectors';
 import { CurrencyDataType } from '@type/currency';
 
-type HandleChangeQueryType = <T>(value: T, inputType?: string) => void;
-
-export const CurrencySearch = () => {
+export const SearchInput = () => {
   const dispatch = useAppDispatch();
   const filteredOptions: CurrencyDataType[] = useSelector(filteredOptionsSelector);
   const searchQuery: string = useSelector(searchQuerySelector);
   const [isListOpen, setIsListOpen] = useState(false);
 
-  const handleChangeQuery: HandleChangeQueryType = (value, inputType = 'input') => {
+  const handleChangeQuery = (value: string, inputType: string = 'input'): void => {
     if (!isListOpen) setIsListOpen(true);
-    const newValue = inputType === 'input' ? (value as ChangeEvent<HTMLInputElement>).target.value : (value as string);
     if (inputType === 'dropdown') setIsListOpen(false);
-    dispatch(setSearchQuery(newValue));
+    dispatch(setSearchQuery(value));
   };
 
   const handleBlur = () => setIsListOpen(false);
   const preventEvent = (e: MouseEvent<HTMLLIElement>) => e.preventDefault();
+
+  const handleItemClick = (label: string) => () => handleChangeQuery(label, 'dropdown');
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => handleChangeQuery(e.target.value, 'input');
 
   useEffect(() => {
     return () => {
@@ -48,7 +48,7 @@ export const CurrencySearch = () => {
           maxLength={30}
           value={searchQuery}
           onBlur={handleBlur}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => handleChangeQuery(e, 'input')}
+          onChange={handleInputChange}
         />
         <SearchIcon />
       </InputWrapper>
@@ -59,7 +59,7 @@ export const CurrencySearch = () => {
               key={value}
               className="DropdownItem"
               onMouseDown={preventEvent}
-              onClick={() => handleChangeQuery(label, 'dropdown')}
+              onClick={handleItemClick(label)}
             >
               {label}
             </DropdownItem>
