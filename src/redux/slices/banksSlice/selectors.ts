a4f539e56eae473d199272ce89044c40e117e721
VMang0 <valeriakorolkova2703@gmail.com>
2024-07-04 02:22:59 +0300

    refactor: clear action and add createSelector for filter

--- src/redux/slices/banksSlice/selectors.ts
+++ src/redux/slices/banksSlice/selectors.ts
@@ -1,4 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+import { currencyData } from '@constants/currency';
 import { RootState } from '@redux/store';
 
 export const searchQuerySelector = (state: RootState) => state.banks.searchQuery;
-export const filteredOptionsSelector = (state: RootState) => state.banks.filteredOptions;
+
+export const filteredOptionsSelector = createSelector([searchQuerySelector], (searchQuery) => {
+  if (!searchQuery) return currencyData;
+  return currencyData.filter(
+    (option) =>
+      option.value.includes(searchQuery.toUpperCase()) ||
+      option.label.toUpperCase().includes(searchQuery.toUpperCase()),
+  );
+});
